423b425f1a8f7a85b30059a035a515ebd3fe9839
ktibo <ktibo@mail.ru>
2024-08-13 17:13:26 +1000

    initial commit

---
+++ src/main/java/org/shurygin/domain/Continent.java
@@ -0,0 +1,11 @@
+package org.shurygin.domain;
+
+public enum Continent {
+    ASIA,
+    EUROPE,
+    NORTH_AMERICA,
+    AFRICA,
+    OCEANIA,
+    ANTARCTICA,
+    SOUTH_AMERICA
+}
---
+++ src/main/java/org/shurygin/redis/CityCountry.java
@@ -0,0 +1,39 @@
+package org.shurygin.redis;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.shurygin.domain.Continent;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
+@Getter
+@Setter
+public class CityCountry {
+
+    private Integer id;
+
+    private String name;
+
+    private String district;
+
+    private Integer population;
+
+    private String countryCode;
+
+    private String alternativeCountryCode;
+
+    private String countryName;
+
+    private Continent continent;
+
+    private String countryRegion;
+
+    private BigDecimal countrySurfaceArea;
+
+    private Integer countryPopulation;
+
+    private Set<Language> languages;
+
+}
